11db12c63d20c43fa88719fab354ed6e3ab0326a
Noé <noe.favier@outlook.com>
2024-05-16 09:37:59 +0200

    feat(py): py mod

--- module/orderModule/main.py
+++ module/orderModule/main.py
@@ -1,2 +1,11 @@
-if __name__ == "main":
-    print("Hello from Python")
+from flask import Flask
+# pip install Flask
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def hello_world():
+    return '{"message": "Hello from orderModule"}'
+
+if __name__ == '__main__':
+    app.run(debug=True)
