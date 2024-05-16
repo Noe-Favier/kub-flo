5d8443614c1f0b738effcb9d067168360d0a97d6
Noé <noe.favier@outlook.com>
2024-05-16 09:41:06 +0200

    fix(js): logging

--- module/productModule/index.js
+++ module/productModule/index.js
@@ -1,9 +1,10 @@
 const http = require("http");
 
+console.log("[SERVER]: Starting productModule server on :8080");
 http
     .createServer(function (req, res) {
-        res.write("Hello World!"); //write a response to the client
-        console.log("object mon gars");
+        res.write(JSON.stringify({message: "Hello from productModule"})); //write a response to the client
+        console.log("[SERVER]: New Request: ", req.url);
         res.end(); //end the response
     })
     .listen(8080); //the server object listens on port 8080
