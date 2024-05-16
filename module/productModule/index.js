e6d6a16fd7a000e70d21d236a9b60df44bf7b804
Florian HAVARD <florian.havard@gmail.com>
2024-05-16 09:31:52 +0200

    fix(Node): respons api

--- module/productModule/index.js
+++ module/productModule/index.js
@@ -1,1 +1,9 @@
-console.log("Hello from JS")
+const http = require("http");
+
+http
+    .createServer(function (req, res) {
+        res.write("Hello World!"); //write a response to the client
+        console.log("object mon gars");
+        res.end(); //end the response
+    })
+    .listen(8080); //the server object listens on port 8080
